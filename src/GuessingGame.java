0480bcc20eb823d1099a62e2bf4a20e198a6a726
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-05-26 19:08:58 -0300

    feat: add colors to the game.

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -5,17 +5,28 @@ public class GuessingGame {
 
     private int number;
     private int attempts;
+    private int maxAttempts;
     private String result;
     private Scanner input;
     private boolean isEnd;
 
+    private final String VERMELHO = "\u001B[31m";
+    private final String VERDE = "\u001B[32m";
+
     public GuessingGame() {
         this.input = new Scanner(System.in);
-        System.out.println("Bem-vindo ao jogo de adivinhação! \n");
+        System.out.println(VERDE + "Bem-vindo ao jogo de adivinhação! \n");
         generateRandomNum();
         this.attempts = 10;
-        System.out.println("Tente adivinhar um número entre 1 e 100");
+        String text = colorTextRed("1");
+        String anotherText = colorTextRed("100");
+        System.out.println(String.format("Tente adivinhar um número entre %s %s %s \n", text,
+                "e", anotherText));
         choiceDifficulty();
+    }
+
+    private String colorTextRed(String text) {
+        return VERMELHO + text + VERDE;
     }
 
     /**
@@ -26,28 +37,40 @@ public class GuessingGame {
         try {
 
             System.out.println("Escolha sua dificuldade.");
-            System.out.println("Caso deseje um desafio mais tranquilo, digite 1.");
-            System.out.println("Caso deseje um desafio moderado, digite 2.");
-            System.out.println("Caso deseje um desafio desafior, digite 3. \n");
+            System.out.println(String.format(
+                    "Caso deseje um desafio mais tranquilo, digite %s.",
+                    colorTextRed("1")));
+            System.out.println(String.format(
+                    "Caso deseje um desafio moderado, digite %s.",
+                    colorTextRed("2")));
+            System.out.println(String.format(
+                    "Caso deseje um desafio desafior, digite %s. \n",
+                    colorTextRed("3")));
 
-            System.out.print("Escolha sua dificuldade:");
+            System.out.print("Escolha sua dificuldade: " + VERMELHO);
             int number = this.input.nextInt();
 
             switch (number) {
                 case 1:
+                    this.maxAttempts = 15;
                     this.attempts = 15;
                     break;
                 case 2:
+                    this.maxAttempts = 10;
                     this.attempts = 10;
                     break;
                 default:
+                    this.maxAttempts = 5;
                     this.attempts = 5;
                     break;
             }
 
-            System.out.println(String.format("Você tem %d tentativas. \n", this.attempts));
+            String attemptsText = colorTextRed(Integer.toString(this.attempts, 10));
+
+            System.out.println(String.format("%sVocê tem %s tentativas. \n", VERDE,
+                    attemptsText));
         } catch (Exception e) {
-            System.out.println("Digite um número!");
+            System.out.println(VERMELHO + "Digite um número!");
         }
     }
 
@@ -58,28 +81,30 @@ public class GuessingGame {
     }
 
     private void checkResult(int number) {
+        String numberText = colorTextRed(Integer.toString(number, 10));
         if (number < this.number) {
-            this.result = String.format("O número é maior do que %d \n", number);
+            this.result = String.format(VERDE + "O número é maior do que %s \n", numberText);
         } else if (number > this.number) {
-            this.result = String.format("O número é menor do que %d \n", number);
+            this.result = String.format(VERDE + " O número é menor do que %s \n", numberText);
         } else {
             this.isEnd = true;
-            this.result = String.format("Parabéns! Você acertou o número em %d tentativas. \n", (10 - this.attempts));
+            this.result = String.format(VERDE + "Parabéns! Você acertou o número em %s tentativas. \n",
+                    colorTextRed(Integer.toString((this.maxAttempts - this.attempts + 1), 10)));
         }
     }
 
     private void checkAttempts() {
         if (this.attempts == 0) {
-            this.result = "O número de tentativas acabaram";
+            this.result = String.format("O número de tentativas acabaram! o resultado era %d", this.number);
             this.isEnd = true;
         } else {
-            this.attempts = this.attempts - 1;
+            this.attempts -= 1;
         }
     }
 
     public void getResult() {
         try {
-            System.out.print("Digite seu palpite:");
+            System.out.print(VERMELHO + "Digite seu palpite: ");
             int number = this.input.nextInt();
             checkResult(number);
             checkAttempts();
